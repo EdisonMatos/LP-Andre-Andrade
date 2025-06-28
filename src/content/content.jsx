3d26652f57eb422d3fdbc1a3758b12bc01e6dccf
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-28 09:00:10 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -123,20 +123,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO EM IBITINGA - SP",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Você não precisa de
+          <span className="text-primary"> um advogado…</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "…até o dia em que tudo desmorona e só quem entende de lei pode te manter de pé.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Descubra por quê",
       obsHero: {
         icon: (
           <svg
@@ -156,7 +157,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento exclusivamente online",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
